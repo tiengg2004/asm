da5858181e989c789ca0e17ee8e4499af3b6716a
your name <johndoe@example.com>
2024-05-22 16:35:26 +0700

    Thông điệp commit của bạn

---
+++ ASM/src/main/java/com/laemcode/Model/User.java
@@ -0,0 +1,13 @@
+package com.laemcode.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	int id;
+	String name;
+}
---
+++ ASM/src/main/java/com/laemcode/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.laemcode.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+}
